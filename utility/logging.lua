8a0b729dc52b21b784f3cb2939928ee6e7c737cf
muppet9010 <40836017+muppet9010@users.noreply.github.com>
2019-12-15 18:16:44 +0000

    update utils and new version

--- utility/logging.lua
+++ utility/logging.lua
@@ -1,4 +1,5 @@
 local Logging = {}
+local Constants = require("constants")
 
 function Logging.PositionToString(position)
     if position == nil then
@@ -7,12 +8,25 @@ function Logging.PositionToString(position)
     return "(" .. position.x .. ", " .. position.y .. ")"
 end
 
+function Logging.BoundingBoxToString(boundingBox)
+    if boundingBox == nil then
+        return "nil boundingBox"
+    end
+    return "((" .. boundingBox.left_top.x .. ", " .. boundingBox.left_top.y .. "), (" .. boundingBox.right_bottom.x .. ", " .. boundingBox.right_bottom.y .. "))"
+end
+
 function Logging.Log(text, enabled)
     if enabled ~= nil and not enabled then
         return
     end
     if game ~= nil then
-        game.write_file("Inbuilt_Lighting_logOutput.txt", tostring(text) .. "\r\n", true)
+        if Constants.LogFileName == nil or Constants.LogFileName == "" then
+            game.print("ERROR - No Constants.LogFileName set")
+            log("ERROR - No Constants.LogFileName set")
+        end
+        game.write_file(Constants.LogFileName, tostring(text) .. "\r\n", true)
+    else
+        log(tostring(text))
     end
 end
 
